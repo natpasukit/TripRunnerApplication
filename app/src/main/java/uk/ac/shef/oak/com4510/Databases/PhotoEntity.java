62e24097a3171e7ab17c4abf71d11638e0eec038
natpa <natpasukit@gmail.com>
2019-12-16 17:26:51 +0000

    Merge from main

---
+++ app/src/main/java/uk/ac/shef/oak/com4510/Databases/PhotoEntity.java
@@ -0,0 +1,32 @@
+package uk.ac.shef.oak.com4510.Databases;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+import uk.ac.shef.oak.com4510.Utils.TimestampConverter;
+
+/**
+ * Define photo database entity
+ * photoId as a primary key
+ * tripId refer to tripId in LocAndSensorData
+ * tripStopId refer to id in LocAndSensorData
+ */
+@Entity(tableName = "photo_information")
+public class PhotoEntity {
+    // Autogenerate photo id
+    @PrimaryKey(autoGenerate = true)
+    public int photoId;
+
+    public int tripId;
+    public int tripStopId;
+    public String photoFileDirectory;
+    /**
+     * @// TODO: Use type converter to create datetime for photo
+     */
+    // ISO8601 strings (“YYYY-MM-DD HH:MM:SS.SSS”)
+    // @TypeConverter({TimestampConverter.class})
+    // public Date lastModified;
+}
---
+++ app/src/main/java/uk/ac/shef/oak/com4510/Utils/TimestampConverter.java
@@ -0,0 +1,31 @@
+package uk.ac.shef.oak.com4510.Utils;
+
+
+import androidx.room.TypeConverter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Converter to convert string into time zone format
+ */
+public class TimestampConverter {
+    static DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.UK);
+
+    @TypeConverter
+    public static Date fromTimestamp(String value) {
+        if (value != null) {
+            try {
+                return df.parse(value);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+}
