12b4d5fa5a6a3c95b98d7c924024b8b9fd483954
Yuncheng Hua <yuncheng@Yunchengs-MacBook-Pro.local>
2019-12-18 22:17:35 +0000

    add intent service for location tracking

--- app/src/main/java/uk/ac/shef/oak/com6510/Models/MyMap.java
+++ app/src/main/java/uk/ac/shef/oak/com6510/Models/MyMap.java
@@ -2,19 +2,31 @@ package uk.ac.shef.oak.com6510.Models;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -27,18 +39,19 @@ import uk.ac.shef.oak.com6510.ViewModels.MapViewModel;
 public class MyMap {
 
     private static final int ACCESS_FINE_LOCATION = 123;
+    private static Barometer barometer;
+    private static Temperature temperature;
+    private static ArrayList<LatLng> latLngs;
+    private static int tripNumber;
+    private static MapViewModel mapViewModel;
+    private static String tripName;
     private LocationRequest mLocationRequest;
     private FusedLocationProviderClient mFusedLocationClient;
     private Context context;
-    private Barometer barometer;
-    private Temperature temperature;
     private boolean started;
     private Location mCurrentLocation;
     private long mLastUpdateTime;
-    private ArrayList<LatLng> latLngs = new ArrayList<LatLng>();
-    private int tripNumber;
-    private MapViewModel mapViewModel;
-    private String tripName;
+    private PendingIntent mLocationPendingIntent;
 
     public MyMap(Context context, String tripName, Barometer barometer, Temperature temperature, MapViewModel mapViewModel) {
         this.context = context;
@@ -48,9 +61,10 @@ public class MyMap {
         this.temperature = temperature;
         this.started = false;
         this.tripNumber = this.mapViewModel.getLatestTripId() + 1;
+        this.latLngs = new ArrayList<LatLng>();
     }
 
-    public void startLocationUpdates() {
+    public boolean startLocationIntentService(){
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
@@ -59,6 +73,7 @@ public class MyMap {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
+                return false;
 
             } else {
 
@@ -67,20 +82,45 @@ public class MyMap {
                 ActivityCompat.requestPermissions((Activity) context,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         ACCESS_FINE_LOCATION);
-
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
 
-            return;
         }
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null /* Looper */);
+
+        startAndInitLocIntentService();
+        return true;
+    }
+
+    private void startAndInitLocIntentService(){
+        Intent intent = new Intent(context, LocationIntent.class);
+        mLocationPendingIntent = PendingIntent.getService(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Task<Void> locationTask = mFusedLocationClient.requestLocationUpdates(mLocationRequest,mLocationPendingIntent);
+        if(locationTask != null){
+            locationTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    if (e instanceof ApiException){
+                        Log.w("Error", ((ApiException) e).getStatusMessage());
+                    }else{
+                        Log.w("Error",e.getMessage());
+                    }
+                }
+            });
+
+            locationTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Log.d("Service","start location intent service successfully");
+                }
+            });
+        }
     }
 
     public void stopLocationUpdates() {
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-        latLngs = new ArrayList<LatLng>();
+        mFusedLocationClient.removeLocationUpdates(mLocationPendingIntent);
     }
 
     public void resume() {
@@ -90,7 +130,7 @@ public class MyMap {
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
 
-        startLocationUpdates();
+        startLocationIntentService();
     }
 
 
@@ -112,6 +152,12 @@ public class MyMap {
         }
     };
 
+    public static void insertNewData(double lat, double lon){
+        latLngs.add(new LatLng(lat, lon));
+        LocAndSensorData l = new LocAndSensorData(System.currentTimeMillis(), temperature.getLatestValue(), barometer.getLatestValue(), barometer.getLatestAccuracy(), lat, lon, tripNumber, tripName);
+        mapViewModel.insertOneData(l);
+    }
+
     public void permissionsResult(int requestCode,
                                   String permissions[], int[] grantResults) {
         switch (requestCode) {
@@ -122,12 +168,12 @@ public class MyMap {
 
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-                            mLocationCallback, null /* Looper */);
+                    startAndInitLocIntentService();
                 } else {
 
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    Log.e("Permission","deny");
                 }
                 return;
             }
@@ -145,7 +191,7 @@ public class MyMap {
         return started;
     }
 
-    public ArrayList<LatLng> getLatLngs() {
+    public static ArrayList<LatLng> getLatLngs() {
         return latLngs;
     }
 }
