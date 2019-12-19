aed71fe237f1eb742c63bc17f9f91bd820384627
acp19yc <ychen312@sheffield.ac.uk>
2019-12-19 12:09:19 +0000

    add a menu at galleryActivity

--- app/src/main/java/uk/ac/shef/oak/com6510/Views/GalleryActivity.java
+++ app/src/main/java/uk/ac/shef/oak/com6510/Views/GalleryActivity.java
@@ -1,11 +1,15 @@
 package uk.ac.shef.oak.com6510.Views;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,5 +53,48 @@ public class GalleryActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sort) {
+
+            sortOptionsDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private  void sortOptionsDialog(){
+
+        String options[] = {"By Ascend","By Path","By List"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sort By:")
+                .setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which==0)
+                        {
+
+                        }
+                        else if (which ==1){
+
+                        }else if (which ==2){
+
+                        }
+                    }
+                }).create().show();
+    }
 }
