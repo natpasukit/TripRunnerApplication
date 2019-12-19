093b6963b22cebe3a66085740e6da49b5c3d2d73
natpa <natpasukit@gmail.com>
2019-12-19 02:58:44 +0000

    Sample cursor loop

--- app/src/main/java/uk/ac/shef/oak/com6510/ViewModels/ImageViewAdapter.java
+++ app/src/main/java/uk/ac/shef/oak/com6510/ViewModels/ImageViewAdapter.java
@@ -12,14 +12,20 @@ public class ImageViewAdapter {
     private Context context;
     private MapRepository mapRepository;
     private Cursor pointsCursorList;
-    private CursorAdapter cursorAdapter;
 
-    public ImageViewAdapter(Application application, Context context,int tripId){
+    public ImageViewAdapter(Application application, Context context, int tripId) {
         this.application = application;
         this.context = context;
         this.mapRepository = new MapRepository(application);
 
         this.pointsCursorList = this.mapRepository.getAllPointsInOneTrip(tripId);
 
+        for (this.pointsCursorList.moveToFirst(); !this.pointsCursorList.isAfterLast(); this.pointsCursorList.moveToNext()) {
+            System.out.println(this.pointsCursorList.getInt(this.pointsCursorList.getColumnIndex("tripId")));
+            System.out.println(this.pointsCursorList.getString(this.pointsCursorList.getColumnIndex("tripName")));
+
+            System.out.println(this.pointsCursorList.getDouble(this.pointsCursorList.getColumnIndex("longitude")));
+            System.out.println(this.pointsCursorList.getDouble(this.pointsCursorList.getColumnIndex("latitude")));
+        }
     }
 }
